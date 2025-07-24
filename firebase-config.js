1ef0fded82a3794744f9a5f19a41fbc5ec5f3c62
inves13 <investh13@gmail.com>
2025-07-24 14:42:09 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzOgL6cvc0b4AO0vAEay1CeSAv_-WqaWQ",
+  authDomain: "saxpag-miner.firebaseapp.com",
+  projectId: "saxpag-miner",
+  storageBucket: "saxpag-miner.firebasestorage.app",
+  messagingSenderId: "154250189626",
+  appId: "1:154250189626:web:83c232a5c539e3f9fe0e03",
+  measurementId: "G-LEV5TL9P7H",
+  databaseURL: "https://saxpag-miner-default-rtdb.firebaseio.com"
+};
 
+firebase.initializeApp(firebaseConfig);
